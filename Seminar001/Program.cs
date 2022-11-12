6f54b469f5cf03421d4b9e5597e277386c51bfa7
Alds <Aldsmsk@gmail.com>
2022-11-12 23:19:03 +0300

    Семинар

---
+++ Seminar001/Program.cs
@@ -0,0 +1,111 @@
+﻿/*
+//Напишите программу, которая на вход принимает число и возводит его в квадрат
+
+// тип данных для переменной = значение
+// int number = 5;
+// // тип данных имя переменной
+// int secondNumber;
+// secondNumber = 5;
+// Консоль выводит результа
+// Console.Write() выводит строчку
+// Сonsole.WriteLine() выводит на следующую строку
+*/
+
+
+// Задача № 1
+
+// int numberOne = Convert.ToInt32(Console.ReadLine());
+// int numberTwo = Convert.ToInt32(Console.ReadLine());
+// if (numberOne * numberOne == numberTwo)
+// {
+//     Console.WriteLine($"Число: {numberTwo} является квадратом числа {numberOne}");
+
+// }
+// else
+// {
+//     Console.WriteLine("Не является квадратом");
+// }
+// Console.WriteLine("Введите число, чтобы узнать день недели: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (number == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (number == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (number == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (number == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (number == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (number == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+//     Console.WriteLine("Дня недели - нет");
+
+// void Squar(int num)
+// {
+//     Console.WriteLine(num * num);
+// }
+// Squar(2);
+// PrintNumber();
+// int number = 6;
+
+// void PrintNumber(int number)
+// {
+//     Console.WriteLine(number * number);
+// }
+
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.WriteLine("Введите число");
+            // int num = Convert.ToInt32(Console.ReadLine());
+
+            // int i = -num;
+            // int max = num;
+            // if(num<0)
+            // { 
+            //     i = num;
+            //     max = -num;
+            // }
+            // Console.WriteLine("");
+            // while (i <= max)
+            // {
+            //     Console.Write($" {i} ");      
+            //     i += 1;
+            // }
+
+            // }
+
+// int num = 8;
+// Console.WriteLine(num);
+// PrintNum(num);
+
+// void PrintNum(int num)
+// {
+//     Console.WriteLine(num = num * num);
+//     Console.WriteLine(num += 10);
+// }
+// int num = Convert.ToInt32(Console.ReadLine());
+// num = Math.Abs(num);
+// int count = - num;
+// while (count <= num)
+// {
+//     Console.Write(count + " ");
+//     count++;
+// }
